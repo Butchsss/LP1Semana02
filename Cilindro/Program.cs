47a3b70329c40e06d51736196928ce5520adbd33
Butchsss <rodrigomarques23work@gmail.com>
2022-03-22 21:09:10 +0000

    Ex04 Added

---
+++ Cilindro/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Cilindro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserir altura");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Inserir raio");
+            int r = int.Parse(Console.ReadLine());
+            float p = 3.1415926f;
+            float v = p*(r*r)*a;
+            float s = 2*p*r*(r+a);
+            Console.WriteLine($"Volume = {v}");
+            Console.WriteLine($"Area de superficie = {s}");  
+        }
+    }
+}
--- IncDec/Program.cs
+++ IncDec/Program.cs
@@ -6,12 +6,19 @@ namespace IncDec
     {
         static void Main(string[] args)
         {
-            int i = 5;
+            /*int i = 5;
             i++;
             Console.WriteLine(i);
             int b = 4;
             --b;
-            Console.WriteLine(b);
+            Console.WriteLine(b);*/
+            int i1 = 3;
+            int i2 = 1;
+            int i3 = -8;
+            int x = 2;
+            x %= i2 - 0b1001;
+            Console.WriteLine(x);
+
         }
     }
 }
