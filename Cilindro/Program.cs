0b05d332e97fddcc3d141bc2b2ffc2e212f891ee
Butchsss <rodrigomarques23work@gmail.com>
2022-03-22 21:16:13 +0000

    Ex05 Added

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -10,11 +10,11 @@ namespace Cilindro
             int a = int.Parse(Console.ReadLine());
             Console.WriteLine("Inserir raio");
             int r = int.Parse(Console.ReadLine());
-            float p = 3.1415926f;
-            float v = p*(r*r)*a;
-            float s = 2*p*r*(r+a);
+            double p = Math.PI;
+            double v = p * Math.Pow(r,r) * a;
+            double s = 2 * p * r * (r + a);
             Console.WriteLine($"Volume = {v}");
-            Console.WriteLine($"Area de superficie = {s}");  
+            Console.WriteLine($"Area de superficie = {s}");
         }
     }
 }
